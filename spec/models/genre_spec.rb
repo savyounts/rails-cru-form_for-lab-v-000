a19770a1a3ffbdf11e5ea05021582a060ece605e
Savannah Younts <savyounts@gmail.com>
2018-09-20 20:00:30 +0000

    automatically backed up by learn

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
   it "creates a new genre" do
-    pop = Genre.new("pop")
+    pop = Genre.new(name: "pop")
 
     expect(pop.name).to equal("pop")
   end
