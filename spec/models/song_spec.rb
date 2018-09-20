16fdf135bb455a0cf9271e3b7a841815f648e350
Savannah Younts <savyounts@gmail.com>
2018-09-20 20:05:39 +0000

    automatically backed up by learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
   it "creates a song" do
-    song = Song.new(name: "song name", artist_id: 1, genre_id: 1)
-    artist = Artist.new(id: 1, name: "adele", bio: "adele bio")
-    genre = Genre.new(id: 1, name: "pop")
+    song = Song.create(name: "song name", artist_id: 1, genre_id: 1)
+    artist = Artist.create(id: 1, name: "adele", bio: "adele bio")
+    genre = Genre.create(id: 1, name: "pop")
 
     expect(song.name).to eq("song name")
     expect(song.artist).to eq(artist)
