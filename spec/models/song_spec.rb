77e9188716fedcf328c789e6a0cdf155f8801824
Savannah Younts <savyounts@gmail.com>
2018-09-20 20:04:06 +0000

    automatically backed up by learn

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates a song" do 
+    song = Song.new(name: "song name", artist_id: 1, genre_id: 1)
+    artist = Artist.new(name: "adele", bio: "adele bio")
+    genre = Genre.new(name: "pop")
+    
+    expect(song.name).to eq("song name")
+    expect(song.artist).to eq(artist)
+    expect(song.genre).to eq(genre)
+  end
 end
