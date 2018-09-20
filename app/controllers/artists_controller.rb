ccd0de5aff0a6fadee65235a161e8e200d2c9803
Savannah Younts <savyounts@gmail.com>
2018-09-20 19:40:18 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -20,7 +20,7 @@ class ArtistsController < ApplicationController
   def update
     set_artist
     @artist.update(artist_params(:name, :bio))
-    redirect artist_path(@artist)
+    redirect_to artist_path(@artist)
   end
 
   def show
